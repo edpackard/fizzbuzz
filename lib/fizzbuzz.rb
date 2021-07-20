8694c0827136e5bc5acd1bc08840f905df1e10e2
ian <kickthechair@gmail.com>
2021-07-20 15:16:06 +0000

    added in edgecase to make sure number is an integer

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(number)
-  if (number % 3).zero? && (number % 5).zero?
-      'fizzbuzz'
+  if !number.is_a?(Integer)
+    puts "Please enter an integer"
+  elsif (number % 3).zero? && (number % 5).zero?
+    'fizzbuzz'
   elsif (number % 3).zero?
     'fizz'
   elsif (number % 5).zero?
