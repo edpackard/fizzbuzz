0b007f5ea5242cc1929fb6e667f180beea592753
ian <kickthechair@gmail.com>
2021-07-20 14:31:31 +0000

    added test for a number that isnt a multiple of 3, 5 or 3 and 5.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,3 +18,9 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns argument number when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
